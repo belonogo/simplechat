386d41ddfebb7b576a5db8261482c584a775f9f7
Pablo <74504296+belonogo@users.noreply.github.com>
2024-08-06 11:20:11 +0300

    Update server.h

--- server/server.h
+++ server/server.h
@@ -26,11 +26,13 @@ private slots:
 private:
     QSet<QTcpSocket*> clients; // Список подключенных клиентов
     QMap<QTcpSocket*, QString> userMap; // Отображение клиентов на имена пользователей
+    QSet<QString> activeSessions; // Активные сессии пользователей
 
     void processMessage(QTcpSocket *client, const QString &message);
     void registerUser(QTcpSocket *client, const QString &username, const QString &password);
     void loginUser(QTcpSocket *client, const QString &username, const QString &password);
     void broadcastMessage(const QString &sender, const QString &message);
+    void updateClientList();
     void logAction(const QString &action);
 
     const QString userFilePath = "users.txt"; // Путь к файлу с пользователями
